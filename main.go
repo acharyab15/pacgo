1f39e5090cb27457fe67a49a5c44ec0d41403ee8
Bipeen Acharya <acharyab@seas.upenn.edu>
2019-07-29 09:30:58 -0700

    1: Input and Output

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	// initialize game
 
+	err := loadMaze()
+	if err != nil {
+		log.Printf("Error loading maze: %v\n", err)
+		return
+	}
 	// load resources
 
 	// game loop
 	for {
 		// update screen
+		printScreen()
 
 		// process input
 
@@ -24,5 +35,29 @@ func main() {
 		break
 
 		// repeat
+	}
+}
+
+var maze []string
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
 	}
 }
